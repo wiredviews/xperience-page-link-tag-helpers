c2d66e794a2ad4a81732684f3bb4705209b87004
Sean Wright <sean@wiredviews.com>
2022-04-30 12:07:48 -0400

    feat(lib): add error log for missing page

--- src/XperienceCommunity.PageLinkTagHelpers/PageLinkTagHelper.cs
+++ src/XperienceCommunity.PageLinkTagHelpers/PageLinkTagHelper.cs
@@ -1,15 +1,22 @@
 using System.Collections.Specialized;
+using CMS.Core;
 using CMS.DocumentEngine;
+using CMS.DocumentEngine.Routing;
 using Kentico.Content.Web.Mvc;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace XperienceCommunity.PageLinkTagHelpers;
 
+/// <summary>
+/// Generates a link to a page based on the page's <see cref="TreeNode.NodeGUID"/>.
+/// </summary>
 [HtmlTargetElement("a", Attributes = "xp-page-link")]
 public class PageLinkTagHelper : TagHelper
 {
     private readonly IPageRetriever pageRetriever;
     private readonly IPageUrlRetriever urlRetriever;
+    private readonly IEventLogService log;
+
 
     [HtmlAttributeName("xp-page-link")]
     public ILinkablePage? Page { get; set; }
@@ -20,8 +27,9 @@ public class PageLinkTagHelper : TagHelper
     [HtmlAttributeName("xp-page-link-query-params")]
     public NameValueCollection? QueryParams { get; set; }
 
-    public PageLinkTagHelper(IPageRetriever pageRetriever, IPageUrlRetriever urlRetriever)
+    public PageLinkTagHelper(IPageRetriever pageRetriever, IPageUrlRetriever urlRetriever, IEventLogService log)
     {
+        this.log = log;
         this.pageRetriever = pageRetriever;
         this.urlRetriever = urlRetriever;
     }
@@ -35,13 +43,21 @@ public class PageLinkTagHelper : TagHelper
 
         var pages = await pageRetriever
             .RetrieveAsync<TreeNode>(
-                q => q.WhereEquals(nameof(TreeNode), Page.NodeGUID),
+                query => query
+                    .WhereEquals(nameof(TreeNode), Page.NodeGUID)
+                    .Columns(nameof(TreeNode.NodeID))
+                    .WithPageUrlPaths(),
                 cache => cache.Key($"page-link|{Page.NodeGUID}"));
 
         var linkedPage = pages.FirstOrDefault();
 
         if (linkedPage is null)
         {
+            log.LogWarning(
+                nameof(PageLinkTagHelper), 
+                $"MISSING_PAGE_{Page.NodeGUID}", // Include the NodeGUID in the EventCode because it is part of the cache key for "ONLY_ONCE" events
+                $"Could not find Page [{Page.NodeGUID}]", loggingPolicy: LoggingPolicy.ONLY_ONCE);
+
             return;
         }
 
